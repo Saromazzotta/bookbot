7050caf3f73337fac86b0ea32f5137eb7014d831
Saromazzotta <saromazzotta@gmail.com>
2024-03-09 11:03:22 -0500

    added in report functionality

---
+++ main.py
@@ -0,0 +1,60 @@
+def main(): 
+    # store book path in a variable
+    book_path = "books/frankenstein.txt"
+
+    #store a variable that opens the book from it's path
+    text = get_book_text(book_path)
+
+    #store num of words in a variable using function that splits the text and returns the len of words
+    num_words = get_num_words(text)
+
+    # Count the occurences of each character in the text
+    char_counts = get_num_appears(text)
+
+    #prints out num_words
+    
+
+    print(char_counts)
+
+    char_list = []
+    for c in char_counts:
+        char_list.append({"char": c, "num": char_counts[c]})
+
+
+    char_list.sort(key=sort_on, reverse=True)
+
+    print(f"--- Begin report of '{book_path}' ---")
+    print(f"{num_words} words found in the document")
+    print()
+    # iterate over now sorted char_list
+    for char in char_list:
+        print(f"The '{char['char']}' character was found {char['num']} times")
+    print("--- End report ---")
+
+
+
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_num_appears(text):
+    count_dictionary = {}
+    for char in text:
+        char = char.lower()
+        if char.isalpha():
+            if char in count_dictionary:
+                count_dictionary[char] += 1
+            else:
+                count_dictionary[char] = 1
+    return count_dictionary
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open("books/frankenstein.txt") as f:
+        return f.read()
+
+main()
